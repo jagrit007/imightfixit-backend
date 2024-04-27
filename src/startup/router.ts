78f7aa1d8e33257d02473a5e7590e442842c748d
Jay Rathod <jayprimerathod@gmail.com>
2024-04-27 23:45:04 +0530

    fix

---
+++ src/startup/router.ts
@@ -0,0 +1,13 @@
+import {Express, Request, Response} from 'express';
+import userRouter from '../controllers/user.controller';
+import serviceRouter from '../controllers/service.controller';
+
+const routerSetup = (app: Express) =>
+    app.get('/', async (req: Request, res: Response) => {
+        res.send('Hello Jay!');
+    })
+        .use('/user', userRouter)
+        .use('/service', serviceRouter)
+
+
+export default routerSetup;
